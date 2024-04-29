6277551a47be4841ff007cacd187f79a3946f285
taizul <taizul@itgrowtech.com>
2024-04-29 11:10:50 +0600

    first commit

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import easyocr
+import os
 
 app = FastAPI()
 
@@ -6,6 +8,25 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World 11"}
+
+
+@app.post("/")
+async def root(uploaded_file: UploadFile = File(...)):
+    file_location = f"file/{uploaded_file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(uploaded_file.file.read())
+
+    h = []
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(file_location)
+
+    for (bbox, text, prob) in result:
+        h.append(text)
+        print(f'Text: {text}, Probability: {prob} {bbox}')
+
+    os.remove(file_location)
+
+    return {"result": h}
 
 
 @app.get("/hello/{name}")
